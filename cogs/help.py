5165f208a19cebc3e54d3681085ec8b72b0e6b94
seriaati <seria.ati@gmail.com>
2023-01-09 10:19:14 +0800

    fix help command

--- cogs/help.py
+++ cogs/help.py
@@ -1,6 +1,7 @@
-from discord.ui import Select
-from discord import SelectOption, app_commands, Interaction, Embed
+from discord import Interaction, SelectOption, app_commands
 from discord.ext import commands
+from discord.ui import Select
+
 from debug import DefaultView
 from utility.utils import default_embed
 
@@ -8,35 +9,47 @@ from utility.utils import default_embed
 class Dropdown(Select):
     def __init__(self, bot: commands.Bot):
         options = [
-            SelectOption(label='flow系統', description='交易方式, 發布委託等',
-                         emoji='🌊'),
-            SelectOption(label='其他', description='其他指令', emoji='🙂'),
-            SelectOption(label='語音台', description='語音台相關指令',
-                         emoji='🎙️'),
-            SelectOption(label='音樂系統', description='音樂系統相關指令',
-                         emoji='🎵')
+            SelectOption(label="flow系統", description="交易方式, 發布委託等", emoji="🌊"),
+            SelectOption(label="其他", description="其他指令", emoji="🙂"),
+            SelectOption(label="語音台", description="語音台相關指令", emoji="🎙️"),
+            SelectOption(label="音樂系統", description="音樂系統相關指令", emoji="🎵"),
         ]
-        super().__init__(placeholder='你想要什麼樣的幫助呢?', options=options)
+        super().__init__(placeholder="你想要什麼樣的幫助呢?", options=options)
         self.bot = bot
 
-    async def callback(self, interaction: Interaction):
-        cogs = ['flow', 'othercmd', 'voice', 'music']
+    async def callback(self, i: Interaction):
+        index = 0
+        cogs = ["flow", "othercmd", "voice", "music"]
         for index, option in enumerate(self.options):
             if option.value == self.values[0]:
                 selected_option = option
                 index = index
                 break
-        embed = default_embed(
-            f'{selected_option.emoji} {selected_option.label}', selected_option.description)
-        commands = self.bot.get_cog(cogs[index]).__cog_app_commands__
+        command_cog = self.bot.get_cog(cogs[index])
+        if command_cog is None:
+            raise ValueError(f"Cog {cogs[index]} not found")
+        commands = command_cog.__cog_app_commands__
+        is_group = command_cog.__cog_is_app_commands_group__
+        group_name = command_cog.__cog_group_name__
+        app_commands = await self.bot.tree.fetch_commands()
+        app_command_dict = {}
+        for app_command in app_commands:
+            app_command_dict[app_command.name] = app_command.id
+
+        embed = default_embed(f"{selected_option.emoji} {selected_option.label}")
         for command in commands:
-            if len(command.checks) != 0:
-                continue
-            embed.add_field(
-                name=f'`{command.name}`',
-                value=command.description
-            )
-        await interaction.response.send_message(embed=embed, ephemeral=True)
+            value = command.description
+            if is_group:
+                embed.add_field(
+                    name=f"</{group_name} {command.name}:{app_command_dict[group_name]}>",
+                    value=value,
+                )
+            else:
+                embed.add_field(
+                    name=f"</{command.name}:{app_command_dict[command.name]}>",
+                    value=value,
+                )
+        await i.response.edit_message(embed=embed)
 
 
 class DropdownView(DefaultView):
@@ -49,18 +62,7 @@ class HelpCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    def returnHelpEmbed(embed: Embed, commands: list[app_commands.Command]):
-        embed = embed
-        for command in commands:
-            if len(command.checks) != 0:
-                continue
-            embed.add_field(
-                name=f'`{command.name}`',
-                value=command.description
-            )
-        return embed
-
-    @app_commands.command(name='help', description='獲得幫助')
+    @app_commands.command(name="help", description="獲得幫助")
     async def help(self, interaction: Interaction):
         view = DropdownView(self.bot)
         await interaction.response.send_message(view=view)
