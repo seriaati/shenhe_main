e38e1e95f256c3a8886a3bdb0142983fcbaa8f86
seriaati <seria.ati@gmail.com>
2023-01-11 18:47:25 +0800

    fix

--- cogs/othercmd.py
+++ cogs/othercmd.py
@@ -9,7 +9,7 @@ from utility.utils import default_embed, error_embed, log
 
 
 class OtherCMDCog(commands.Cog, name="other"):
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.quote_ctx_menu = app_commands.ContextMenu(
             name="語錄", callback=self.quote_context_menu
@@ -24,14 +24,17 @@ class OtherCMDCog(commands.Cog, name="other"):
         self.bot.tree.remove_command(
             self.quote_ctx_menu.name, type=self.quote_ctx_menu.type
         )
+        self.bot.tree.remove_command(
+            self.hao_se_o_ctx_menu.name, type=self.hao_se_o_ctx_menu.type
+        )
 
     async def hao_se_o_context_menu(self, i: Interaction, message: Message):
         c = await i.client.db.cursor()
         await i.response.send_message("已新增", ephemeral=True)
         emojis = [
-            "<:word_hao:1021424223199187025>",
-            "<:word_se:1021424220976193646>",
-            "<:word_o:1021424218337984545>",
+            "<:1_:1062180387922645082>",
+            "<:2_:1062180392246980638>",
+            "<:3_:1062180394906177678>",
         ]
         for e in emojis:
             await message.add_reaction(e)
@@ -98,27 +101,23 @@ class OtherCMDCog(commands.Cog, name="other"):
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
-        if payload.emoji.id == 966578081139662939: # QuoteTimeWakuWaku
-            if payload.channel_id == 965842415913152522:
-                return await self.bot.get_channel(payload.channel_id).send(
-                    embed=error_embed().set_author(name="不可以在色色台語錄唷"), delete_after=3
-                )
+        if payload.emoji.id == 1062180398077059132: # QuoteTimeWakuWaku
             log(True, False, "Quote", payload.user_id)
             member = self.bot.get_user(payload.user_id)
-            channel = self.bot.get_channel(payload.channel_id)
             msg = await channel.fetch_message(payload.message_id)
             channel = self.bot.get_channel(payload.channel_id)
             emoji = self.bot.get_emoji(payload.emoji.id)
             await msg.remove_reaction(emoji, member)
             await channel.send(
-                f"<a:check_animated:982579879239352370> 語錄擷取成功", delete_after=3
+                f"✅ 語錄擷取成功", delete_after=3
             )
             embed = default_embed(
                 f"語錄",
-                f"「{msg.content}」\n  -{msg.author.mention}\n\n[點我回到該訊息]({msg.jump_url})",
+                f"{msg.content}\n\n[點我回到該訊息]({msg.jump_url})",
             )
             embed.set_thumbnail(url=str(msg.author.avatar))
-            channel = self.bot.get_channel(966549110540877875)
+            embed.set_author(name=member.display_name)
+            channel = self.bot.get_channel(1061883645591310427)
             await channel.send(embed=embed)
 
     @app_commands.command(name="ping延遲", description="查看機器人目前延遲")
@@ -151,20 +150,17 @@ class OtherCMDCog(commands.Cog, name="other"):
 
     @commands.command(aliases=["q"])
     async def quote(self, ctx):
-        if ctx.message.channel.id == 965842415913152522:
-            return await ctx.send(
-                embed=error_embed().set_author(name="不可以在色色台語錄唷"), delete_after=3
-            )
         log(True, False, "Quote", ctx.author.id)
         await ctx.message.delete()
         msg = await ctx.channel.fetch_message(ctx.message.reference.message_id)
         embed = default_embed(
             f"語錄",
-            f"「{msg.content}」\n  -{msg.author.mention}\n\n[點我回到該訊息]({msg.jump_url})",
+            f"{msg.content}\n\n[點我回到該訊息]({msg.jump_url})",
         )
         embed.set_thumbnail(url=str(msg.author.avatar))
+        embed.set_author(name=ctx.author.display_name)
         channel = self.bot.get_channel(966549110540877875)
-        await ctx.send("<a:check_animated:982579879239352370> 語錄擷取成功", delete_after=3)
+        await ctx.send("✅ 語錄擷取成功", delete_after=3)
         await channel.send(embed=embed)
 
     @app_commands.command(name="pickrandom")
@@ -206,17 +202,13 @@ class OtherCMDCog(commands.Cog, name="other"):
         )
 
     async def quote_context_menu(self, i: Interaction, msg: Message) -> None:
-        if msg.channel.id == 965842415913152522:
-            return await i.response.send_message(
-                embed=error_embed().set_author(name="不可以在色色台語錄唷"), ephemeral=True
-            )
         log(True, False, "Quote", i.user.id)
         embed = default_embed(
             f"語錄",
-            f"「{msg.content}」\n  -{msg.author.mention}\n\n[點我回到該訊息]({msg.jump_url})",
+            f"{msg.content}\n\n[點我回到該訊息]({msg.jump_url})",
         )
         embed.set_thumbnail(url=str(msg.author.avatar))
-        channel = self.bot.get_channel(966549110540877875)
+        embed.set_author(name=i.user.display_name)
         await i.response.send_message(
             embed=default_embed().set_author(name="語錄擷取成功", icon_url=i.user.display_avatar.url),
             ephemeral=True,
