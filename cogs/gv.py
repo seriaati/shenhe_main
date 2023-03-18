1f058eba02e853d3ce2bca5a5766f322898836cc
seriaati <seria.ati@gmail.com>
2023-03-18 21:44:55 +0800

    feat: add giveaway feature

--- cogs/gv.py
+++ cogs/gv.py
@@ -1,24 +1,154 @@
-from apps.giveaway import View, return_giveaway_embed
-from discord import Interaction, app_commands, SelectOption
+import random
+import typing
+
+import discord
+from discord import app_commands, ui
 from discord.ext import commands
+
+from utility.paginators.paginator import GeneralPaginator
+from utility.utils import default_embed
+
+
+def create_gv_embed(
+    prize: str,
+    author: discord.Member,
+    prize_num: int,
+    extra_info: typing.Optional[str] = None,
+) -> discord.Embed:
+    embed = default_embed(prize, "點按 🎉 按鈕來參加抽獎！")
+    embed.add_field(name="主辦人", value=author.mention, inline=False)
+    embed.add_field(name="獎品數量", value=str(prize_num), inline=False)
+    if extra_info:
+        embed.add_field(name="其他資訊", value=extra_info, inline=False)
+
+    return embed
+
+
+class GiveAwayView(ui.View):
+    def __init__(
+        self,
+        prize: str,
+        author: discord.Member,
+        prize_num: int,
+        extra_info: typing.Optional[str] = None,
+    ) -> None:
+        super().__init__(timeout=None)
+
+        self.participants: typing.List[discord.Member] = []
+        self.prize = prize
+        self.author = author
+        self.prize_num = prize_num
+        self.extra_info = extra_info
+
+    async def update_embed_and_view(self, i: discord.Interaction):
+        embed = create_gv_embed(
+            self.prize, self.author, self.prize_num, self.extra_info
+        )
+        await i.response.edit_message(embed=embed, view=self)
+
+    async def announce_winners(self, i: discord.Interaction):
+        winners = random.sample(self.participants, self.prize_num)
+
+        embed = i.message.embeds[0]
+        embed.color = discord.Color.red()
+        embed.clear_fields()
+        embed.add_field(name="主辦人", value=self.author.mention, inline=False)
+        embed.add_field(name="得獎者", value="\n".join(w.mention for w in winners))
+
+        self.join_gv.disabled = True
+        await i.response.edit_message(embed=embed, view=self, content="**🎊 抽獎結束！ 🎊**")
+
+        winner_mentions = ", ".join(w.mention for w in winners)
+        winner_embed = default_embed(
+            message=f"恭喜 {winner_mentions} 贏得了 [{self.prize}]({i.message.jump_url})！"
+        )
+        await i.channel.send(
+            embed=winner_embed,
+            content=f"恭喜 {winner_mentions} 🎉",
+        )
+
+    @ui.button(
+        style=discord.ButtonStyle.blurple, custom_id="join_gv", emoji="🎉", label="0"
+    )
+    async def join_gv(self, i: discord.Interaction, button: ui.Button):
+        if i.user in self.participants:
+            self.participants.remove(i.user)
+        else:
+            self.participants.append(i.user)
+        button.label = str(len(self.participants))
+        await self.update_embed_and_view(i)
+
+    @ui.button(
+        label="參加者",
+        style=discord.ButtonStyle.grey,
+        custom_id="participants_gv",
+        emoji="👥",
+    )
+    async def participants_gv(self, i: discord.Interaction, _: ui.Button):
+        if not self.participants:
+            await i.response.send_message("當前沒有任何人參加抽獎", ephemeral=True)
+        else:
+            # 5 participants per embed
+            embeds: typing.List[discord.Embed] = []
+            for index in range(0, len(self.participants), 5):
+                description = "\n".join(
+                    p.mention for p in self.participants[index : index + 5]
+                )
+                embed = default_embed(
+                    "參加者",
+                    f"{description}\n\n共 **{len(self.participants)}** 位參加者",
+                )
+                embeds.append(embed)
+
+            await GeneralPaginator(i, embeds).start(ephemeral=True)
+
+    @ui.button(
+        label="結束抽獎", style=discord.ButtonStyle.red, custom_id="end_gv", emoji="🎊"
+    )
+    async def end_gv(self, i: discord.Interaction, button: ui.Button):
+        if i.user.id != self.author.id:
+            await i.response.send_message("你不是主辦人，無法結束抽獎", ephemeral=True)
+        else:
+            if not self.participants:
+                await i.response.send_message("沒有人參加抽獎，無法結束", ephemeral=True)
+            else:
+                self.remove_item(button)
+                await self.announce_winners(i)
+                self.add_item(RerollWinners())
+                await i.edit_original_response(view=self)
+
+
+class RerollWinners(ui.Button[GiveAwayView]):
+    def __init__(self) -> None:
+        super().__init__(
+            label="重新抽獎",
+            style=discord.ButtonStyle.green,
+            custom_id="reroll_winners",
+            emoji="🎲",
+        )
+
+    async def callback(self, i: discord.Interaction) -> typing.Any:
+        if i.user.id != self.author.id:
+            await i.response.send_message("你不是主辦人，無法結束抽獎", ephemeral=True)
+        else:
+            await self.view.announce_winners(i)
 
 
 class GiveAwayCog(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
-        self.debug_toggle = self.bot.debug_toggle
-        self.gv_channel_id = (
-            965517075508498452 if not self.debug_toggle else 909595117952856084
-        )
 
-    @app_commands.command(name="gv", description="設置抽獎")
-    @app_commands.checks.has_role("小雪團隊")
-    async def start_giveaway(self, i: Interaction):
-        select_options = []
-        for role in i.guild.roles:
-            if "λ" in role.name:
-                select_options.append(SelectOption(label=role.name, value=role.id))
-        await i.response.send_message(embed=await return_giveaway_embed(i), view=View(select_options))
+    @app_commands.command(name="gv", description="開始一個抽獎")
+    async def gv(
+        self,
+        i: discord.Interaction,
+        prize: str,
+        prize_num: int,
+        extra_info: typing.Optional[str] = None,
+    ):
+        embed = create_gv_embed(prize, i.user, prize_num, extra_info)
+        view = GiveAwayView(prize, i.user, prize_num, extra_info)
+        await i.response.send_message(embed=embed, view=view)
 
 
 async def setup(bot: commands.Bot) -> None:
--- utility/paginators/paginator.py
+++ utility/paginators/paginator.py
@@ -1,157 +1,147 @@
-__all__ = ["GeneralPaginator"]
+import typing
+
+import discord
+from discord import ui
 
 
-from io import BytesIO
-from typing import List, Optional, Union
-
-import aiosqlite
-from discord import ButtonStyle, Embed, File, Interaction, User
-from discord.ui import Button, Select, button, View
-
-
-class _view(View):
+class GeneralPaginatorView(ui.View):
     def __init__(
         self,
-        author: User,
-        embeds: List[Embed],
-        db: aiosqlite.Connection,
-        check: bool = True,
-        files: Optional[List[BytesIO]] = [],
+        embeds: typing.List[discord.Embed],
     ):
-        super().__init__(timeout=500)
-        self.author = author
-        self.embeds = embeds
-        self.check = check
-        self.files = files
+        super().__init__()
 
+        self.embeds = embeds
         self.current_page = 0
 
-    async def interaction_check(self, i: Interaction) -> bool:
-        return i.user.id == self.author.id
+    async def update_children(self, i: discord.Interaction):
+        """Called when a button is pressed"""
+        self.update_components()
+        await self.make_response(i)
 
-    async def update_children(self, interaction: Interaction):
+    def update_components(self):
+        """Update the buttons and the page label"""
+        self.first.disabled = self.current_page == 0
         self.next.disabled = self.current_page + 1 == len(self.embeds)
         self.previous.disabled = self.current_page <= 0
+        self.last.disabled = self.current_page + 1 == len(self.embeds)
 
-        kwargs = {"embed": self.embeds[self.current_page]}
-        if len(self.files) > 0:
-            fp = self.files[self.current_page]
-            fp.seek(0)
-            file_name = f"{self.current_page}.jpeg"
-            file = File(fp, file_name)
-            kwargs["attachments"] = [file]
+        self.page.label = f"第{self.current_page + 1}/{len(self.embeds)}頁"
 
-        kwargs["view"] = self
+    async def make_response(self, i):
+        """Make the response for the interaction"""
+        await i.response.edit_message(embed=self.embeds[self.current_page], view=self)
 
-        await interaction.response.edit_message(**kwargs)
-
-    @button(
+    @ui.button(
         emoji="<:double_left:982588991461281833>",
-        style=ButtonStyle.gray,
+        style=discord.ButtonStyle.gray,
         row=1,
         custom_id="paginator_double_left",
     )
-    async def first(self, interaction: Interaction, button: Button):
+    async def first(self, i: discord.Interaction, _: ui.Button):
         self.current_page = 0
 
-        await self.update_children(interaction)
+        await self.update_children(i)
 
-    @button(
+    @ui.button(
         emoji="<:left:982588994778972171>",
-        style=ButtonStyle.blurple,
+        style=discord.ButtonStyle.blurple,
         row=1,
         custom_id="paginator_left",
     )
-    async def previous(self, interaction: Interaction, button: Button):
+    async def previous(self, i: discord.Interaction, _: ui.Button):
         self.current_page -= 1
 
-        await self.update_children(interaction)
+        await self.update_children(i)
 
-    @button(
+    @ui.button(
+        label="page 1/1",
+        custom_id="paginator_page",
+        disabled=True,
+        row=1,
+    )
+    async def page(self, _: discord.Interaction, _button: ui.Button):
+        """This button is just a label"""
+
+    @ui.button(
         emoji="<:right:982588993122238524>",
-        style=ButtonStyle.blurple,
+        style=discord.ButtonStyle.blurple,
         row=1,
         custom_id="paginator_right",
     )
-    async def next(self, interaction: Interaction, button: Button):
+    async def next(self, i: discord.Interaction, _: ui.Button):
         self.current_page += 1
 
-        await self.update_children(interaction)
+        await self.update_children(i)
 
-    @button(
+    @ui.button(
         emoji="<:double_right:982588990223958047>",
-        style=ButtonStyle.gray,
+        style=discord.ButtonStyle.gray,
         row=1,
         custom_id="paginator_double_right",
     )
-    async def last(self, interaction: Interaction, button: Button):
+    async def last(self, i: discord.Interaction, _: ui.Button):
         self.current_page = len(self.embeds) - 1
 
-        await self.update_children(interaction)
+        await self.update_children(i)
 
 
 class GeneralPaginator:
     def __init__(
         self,
-        interaction: Interaction,
-        embeds: List[Embed],
-        custom_children: Optional[List[Union[Button, Select]]] = [],
-        files: Optional[List[BytesIO]] = [],
+        i: discord.Interaction,
+        embeds: typing.List[discord.Embed],
+        custom_children: typing.Optional[
+            typing.List[typing.Union[ui.Button, ui.Select]]
+        ] = None,
     ):
-        self.custom_children = custom_children
-        self.interaction = interaction
+        if custom_children is None:
+            custom_children = []
+        self.i = i
         self.embeds = embeds
-        self.files = files
+        self.custom_children = custom_children
 
     async def start(
         self,
         edit: bool = False,
         followup: bool = False,
-        check: bool = True,
         ephemeral: bool = False,
-        dm: bool = False
     ) -> None:
-        if not (self.embeds):
+        if not self.embeds:
             raise ValueError("Missing embeds")
 
-        view = _view(self.interaction.user, self.embeds, check, self.files)
-        view.previous.disabled = True if (view.current_page <= 0) else False
-        view.next.disabled = (
-            True if (view.current_page + 1 >= len(self.embeds)) else False
-        )
+        view = self.setup_view()
+        view.author = self.i.user
+        view.first.disabled = view.previous.disabled = True
+        view.last.disabled = view.next.disabled = len(self.embeds) == 1
+
+        view.page.label = f"第{self.current_page + 1}/{len(self.embeds)}頁"
 
         if len(self.custom_children) > 0:
             for child in self.custom_children:
                 view.add_item(child)
 
-        kwargs = {"embed": self.embeds[view.current_page]}
-        if len(self.files) > 0:
-            fp = self.files[view.current_page]
-            fp.seek(0)
-            file_name = f"{view.current_page}.jpeg"
-            file = File(fp, file_name)
-            kwargs["files"] = [file]
-
-        kwargs["view"] = view
-
-        if not edit:
+        kwargs = self.setup_kwargs(view)
+        if ephemeral:
             kwargs["ephemeral"] = ephemeral
 
+        if edit and ephemeral:
+            raise ValueError("Cannot edit the ephemeral status of a message")
+
         if edit:
-            if "files" in kwargs:
-                del kwargs["files"]
-                fp = self.files[view.current_page]
-                fp.seek(0)
-                file = File(fp, f"{view.current_page}.jpeg")
-                kwargs["attachments"] = [file]
-
-            await self.interaction.edit_original_response(**kwargs)
+            await self.i.edit_original_response(**kwargs)
         elif followup:
-            await self.interaction.followup.send(**kwargs)
-        elif dm:
-            del kwargs["ephemeral"]
-            await self.interaction.user.send(**kwargs)
+            await self.i.followup.send(**kwargs)
         else:
-            await self.interaction.response.send_message(**kwargs)
+            await self.i.response.send_message(**kwargs)
 
+        view.message = await self.i.original_response()
         await view.wait()
+
+    def setup_kwargs(self, view: GeneralPaginatorView) -> typing.Dict[str, typing.Any]:
+        kwargs: typing.Dict[str, typing.Any] = {"embed": self.embeds[0], "view": view}
+        return kwargs
+
+    def setup_view(self) -> GeneralPaginatorView:
+        view = GeneralPaginatorView(self.embeds)
+        return view
