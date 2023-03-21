4e4d0753eff5615a2927be80ef99fee5cac7d182
seriaati <seria.ati@gmail.com>
2023-03-21 16:53:48 +0800

    feat: add url spoiler

--- cogs/admin.py
+++ cogs/admin.py
@@ -2,11 +2,20 @@ import importlib
 import io
 import sys
 from typing import List
+import re
 
 import discord
 from discord.ext import commands
 
 from utility.utils import error_embed
+
+
+def detect_url(message: str) -> List[str]:
+    regex = re.compile(
+        r"((http|ftp|https):\/\/)?(www\.)?([a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3})(\/\S*)?"
+    )
+    url = re.findall(regex, message)
+    return [x[0] for x in url]
 
 
 class AdminCog(commands.Cog):
@@ -61,6 +70,16 @@ class AdminCog(commands.Cog):
         if message.guild.id != self.bot.guild_id:
             return
 
+        urls = detect_url(message.content)
+        if message.channel.id and urls:
+            await message.delete()
+            content = ""
+            for url in urls:
+                content += f"||{url}||\n"
+            await message.channel.send(
+                content=f"由 {message.author.mention} 寄出\n" + content
+            )
+
         if message.channel.id == 1061898394446069852 and any(
             not a.is_spoiler() for a in message.attachments
         ):
@@ -81,78 +100,6 @@ class AdminCog(commands.Cog):
             await message.channel.send(
                 content=f"由 <@{message.author.id}> 寄出", files=files
             )
-
-    # @commands.Cog.listener()
-    # async def on_raw_message_delete(self, payload: RawMessageDeleteEvent):
-    #     now = get_dt_now()
-    #     if payload.guild_id != 1061875024136503318:
-    #         return
-    #     if payload.channel_id == 965842415913152522:
-    #         return
-    #     c: TextChannel = (
-    #         self.bot.get_channel(988698669442269184)
-    #         if not self.bot.debug_toggle
-    #         else self.bot.get_channel(909595117952856084)
-    #     )
-    #     if payload.cached_message is not None:
-    #         if payload.cached_message.author.id == self.bot.user.id:
-    #             return
-    #         if payload.cached_message.content == "!q":
-    #             return
-    #         attachment_str = (
-    #             "(含有附件)" if len(payload.cached_message.attachments) != 0 else ""
-    #         )
-    #         embed = default_embed(
-    #             "訊息刪除",
-    #             f"「{payload.cached_message.content} {attachment_str}」\n\n"
-    #             f"用戶: {payload.cached_message.author.mention}\n"
-    #             f"頻道: {payload.cached_message.channel.mention}\n"
-    #             f'時間: {now.strftime("%m/%d/%Y %H:%M:%S")}\n'
-    #             f"附近訊息: {payload.cached_message.jump_url}",
-    #         )
-    #         embed.set_footer(text=f"用戶 ID: {payload.cached_message.author.id}\n")
-    #         embed.set_author(
-    #             name=payload.cached_message.author,
-    #             icon_url=payload.cached_message.author.avatar,
-    #         )
-    #         await c.send(embed=embed)
-    #         if len(payload.cached_message.attachments) != 0:
-    #             for a in payload.cached_message.attachments:
-    #                 await c.send(file=await a.to_file(use_cached=True))
-
-    # @commands.Cog.listener()
-    # async def on_member_join(self, member: Member):
-    #     now = get_dt_now()
-    #     if member.guild.id != 1061875024136503318:
-    #         return
-    #     c: TextChannel = (
-    #         self.bot.get_channel(988698669442269184)
-    #         if not self.bot.debug_toggle
-    #         else self.bot.get_channel(909595117952856084)
-    #     )
-    #     embed = default_embed(
-    #         "進群", f"用戶: {member.mention}\n" f'時間: {now.strftime("%m/%d/%Y %H:%M:%S")}\n'
-    #     )
-    #     embed.set_author(name=member, icon_url=member.avatar)
-    #     embed.set_footer(text=f"用戶 ID: {member.id}")
-    #     await c.send(embed=embed)
-
-    # @commands.Cog.listener()
-    # async def on_member_remove(self, member: Member):
-    #     now = get_dt_now()
-    #     if member.guild.id != 1061875024136503318:
-    #         return
-    #     c: TextChannel = (
-    #         self.bot.get_channel(988698669442269184)
-    #         if not self.bot.debug_toggle
-    #         else self.bot.get_channel(909595117952856084)
-    #     )
-    #     embed = default_embed(
-    #         "退群", f"用戶: {member.mention}\n" f'時間: {now.strftime("%m/%d/%Y %H:%M:%S")}\n'
-    #     )
-    #     embed.set_author(name=member, icon_url=member.avatar)
-    #     embed.set_footer(text=f"用戶 ID: {member.id}")
-    #     await c.send(embed=embed)
 
 
 async def setup(bot: commands.Bot) -> None:
