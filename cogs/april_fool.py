7566ebb0619500ad6e3c027d2caa112287482e77
seriaati <seria.ati@gmail.com>
2023-03-31 21:13:01 +0800

    feat: replace custom emojis with kokomi emojis

--- cogs/april_fool.py
+++ cogs/april_fool.py
@@ -1,4 +1,6 @@
+import re
 from datetime import datetime
+from random import choice, randint
 
 import discord
 from discord.ext import commands
@@ -14,7 +16,32 @@ nii_lang_dict = {
     "嗎": "麼",
     "了": "勒",
     "媽": "麻",
+    "在": "再",
 }
+
+kokomi_emojis = (
+    "<:kokoStare:1061964170117001226>",
+    "<:kokoSugoi:1091307049746444298>",
+    "<:kokoTea:1091307053475188736>",
+    "<:kokoGrumpy:1091307055656226899>",
+    "<:kokoBeg:1091307059158450197>",
+    "<:kokoFacepalm:1091307062438395984>",
+    "<:kokoFlap:1091307064397139998>",
+    "<:kokoHide:1091307068138459136>",
+    "<:kokoLove:1091307071523262494>",
+    "<:kokoTeehee:1091307073960153148>",
+    "<:kokoPeek:1091307077110071326>",
+    "<:kokoPeep:1091307080826224670>",
+    "<:kokoSip:1091307083095359509>",
+    "<:kokoAmazed:1091307086299807774>",
+    "<:kokoPikachu:1091307090066292758>",
+    "<:kokoAyaya:1091307092524150804>",
+    "<:kokoBless:1091307095883776040>",
+    "<:kokoHurts:1091307099541229609>",
+    "<:kokoCry:1091307101692887070>",
+    "<:kokoFork:1091307105237094410>",
+    "<:kokoHi:1091307108684800000>",
+)
 
 
 class AprilFoolCog(commands.Cog):
@@ -40,6 +67,16 @@ class AprilFoolCog(commands.Cog):
             new_content = message.content
             for k, v in nii_lang_dict.items():
                 new_content = new_content.replace(k, v)
+
+            num = randint(1, 100)
+            if num <= 10:
+                new_content += "ww"
+
+            # reaplce all emoji strings with "?"
+            guild_emoji_strings = [f"<:{a.name}:{a.id}>" for a in message.guild.emojis]
+            if not any(a in new_content for a in guild_emoji_strings):
+                new_content = re.sub(r"<:\w+:\d+>", choice(kokomi_emojis), new_content)
+
             await webhook.send(
                 content=new_content,
                 username=message.author.display_name,
