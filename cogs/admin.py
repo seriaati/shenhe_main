ee282c9aea46e9bcec6254f01d0d04116b670225
seriaati <seria.ati@gmail.com>
2023-04-03 11:47:16 +0800

    modify: move auto-spiler to `webhook.py`

--- cogs/admin.py
+++ cogs/admin.py
@@ -58,45 +58,6 @@ class AdminCog(commands.Cog):
         await self.bot.tree.sync()
         await ctx.send("Synced")
 
-    @commands.Cog.listener()
-    async def on_message(self, message: discord.Message):
-        if message.author.id == self.bot.user.id:
-            return
-        if message.guild and message.guild.id != self.bot.guild_id:
-            return
-
-        if message.channel.id == 1061898394446069852 and any(
-            not a.is_spoiler() for a in message.attachments
-        ):
-            assert isinstance(message.channel, discord.TextChannel)
-
-            files: List[discord.File] = []
-            await message.delete()
-
-            for attachment in message.attachments:
-                if not attachment.is_spoiler():
-                    async with self.bot.session.get(attachment.proxy_url) as resp:
-                        bytes_obj = io.BytesIO(await resp.read())
-                        file_ = discord.File(
-                            bytes_obj, filename=attachment.filename, spoiler=True
-                        )
-                        files.append(file_)
-                else:
-                    files.append(await attachment.to_file())
-
-            webhooks = await message.channel.webhooks()
-            if not webhooks:
-                webhook = await message.channel.create_webhook(name="Auto-Spoiler")
-            else:
-                webhook = webhooks[0]
-
-            await webhook.send(
-                content=message.content,
-                files=files,
-                username=message.author.display_name,
-                avatar_url=message.author.display_avatar.url,
-            )
-
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(AdminCog(bot))
