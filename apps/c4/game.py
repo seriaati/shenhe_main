12d985595cd1415bdf3fbd4b74a58a5850b3ca0e
seriaati <seria.ati@gmail.com>
2023-04-06 19:44:42 +0800

    feat: beautify board

--- apps/c4/game.py
+++ apps/c4/game.py
@@ -24,8 +24,8 @@ class ConnectFour:
         )
         embed.description = ""
         for row in self.board:
-            embed.description += " ".join(row) + "\n"
-        embed.description += "1️⃣2️⃣3️⃣4️⃣5️⃣6️⃣7️⃣"
+            embed.description += "|".join(row) + "\n"
+        embed.description += "1️⃣|2️⃣|3️⃣|4️⃣|5️⃣|6️⃣|7️⃣"
         embed.set_footer(text=f"現在是 {self.current_player} 的回合")
         member = self.players[0] if self.current_player == "🟡" else self.players[1]
         embed.set_author(name=member.display_name, icon_url=member.display_avatar.url)
