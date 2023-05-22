a350bed0bb15cef55217b803be8c92326d0c3fdb
seriaati <seria.ati@gmail.com>
2023-05-22 19:06:45 +0800

    fix: remove stupid thing

--- cogs/no_spam.py
+++ cogs/no_spam.py
@@ -1,4 +1,8 @@
+import json
+import logging
+from collections import defaultdict
 from datetime import timedelta
+from typing import DefaultDict
 
 import discord
 from discord.ext import commands
@@ -7,7 +11,9 @@ from discord.ext import commands
 class NoSpam(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot: commands.Bot = bot
-        self.user_messages = {}
+        self.user_messages: DefaultDict[int, DefaultDict[str, list[int]]] = defaultdict(
+            lambda: defaultdict(list)
+        )
 
         self.guild_id = 1061877505067327528
         self.owner_id = 410036441129943050
@@ -77,9 +83,6 @@ class NoSpam(commands.Cog):
                 embed.set_footer(text=f"ID: {member.id}")
                 await self.owner.send(embed=embed)
 
-        # Clear the user's messages
-        messages.clear()
-
     @commands.Cog.listener("on_message")
     async def track_message(self, message: discord.Message) -> None:
         if message.author.bot or message.guild is None:
@@ -89,11 +92,7 @@ class NoSpam(commands.Cog):
 
         user_id = message.author.id
         channel_id = message.channel.id
-        if user_id not in self.user_messages:
-            self.user_messages[user_id] = {}
         messages = self.user_messages[user_id]
-        if message.content not in messages:
-            messages[message.content] = []
         channels = messages[message.content]
         channels.append(channel_id)
 
@@ -106,6 +105,11 @@ class NoSpam(commands.Cog):
     @commands.command(name="cs")
     async def cs(self, ctx: commands.Context):
         await ctx.send(str(self.user_messages))
+    
+    @commands.command(name="hp")
+    async def hp(self, ctx: commands.Context):
+        await ctx.send(str(self.user_messages[ctx.author.id]))
+        
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(NoSpam(bot))
