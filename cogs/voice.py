5b6dfc6d64e31a69fae98961263a9c2f5d7e6ce3
seriaati <seria.ati@gmail.com>
2023-05-30 11:32:06 +0800

    fix(vc); channel delete logic

--- cogs/voice.py
+++ cogs/voice.py
@@ -101,7 +101,7 @@ class VoiceCog(commands.GroupCog, name="vc"):
                     old.id,
                 )
             
-            if old.id != make_vc.id and len(old.members) == 0:
+            if old.id != make_vc.id and len(old.members) -1 == 0:
                 try:
                     await old.delete()
                 except discord.NotFound:
