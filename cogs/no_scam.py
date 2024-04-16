c7840a9416c79463fb75d3e6bb07d212c30e724f
seriaati <seria.ati@gmail.com>
2024-04-16 11:51:46 +0900

    fix(no-scam): Add discord invite link check

--- cogs/no_scam.py
+++ cogs/no_scam.py
@@ -87,7 +87,12 @@ class NoScam(commands.Cog):
 
     @commands.Cog.listener("on_message")
     async def track_message(self, message: discord.Message) -> None:
-        if message.author.bot or message.guild is None or not message.content:
+        if (
+            message.author.bot
+            or message.guild is None
+            or not message.content
+            or "discord.gg" not in message.content
+        ):
             return
 
         user_id = message.author.id
